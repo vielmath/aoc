dba02ada80c3e4848081a7e6d7e11a06562d69a3
Mathieu <mathieu.marchandise@oscaro.com>
2025-12-02 07:00:24 +0100

    2025 2

---
+++ src/y2025/2.clj
@@ -0,0 +1,63 @@
+(ns y2025.2
+  (:require [clojure.string :as str]
+            [tools :refer [read-puzzle read-input submit]]))
+
+(def input (read-input 2025 2))
+
+(defn parse [raw]
+  (->>
+   (-> (str/split-lines raw)
+       first
+       (str/split #","))
+   (map (fn [range] (map Long/parseLong (str/split range #"-"))))
+   ))
+
+(defn invalid? [id]
+  (let [s (str id)
+        c (count s)]
+    (when (even? c)
+      (apply = (partition (/ c 2) s)))))
+
+(defn invalid2? [id]
+  (let [s (str id)
+        c (count s)]
+    (->>
+     (range 1 (inc (quot c 2)))
+     ;(map #(partition-all % s))
+     (map #(apply = (partition-all % s)))
+     (some true?)
+     )))
+
+(defn range->invalids [part [from to]]
+  (->> (range from (inc to))
+       (filter (if (= part 1) invalid? invalid2?))
+       ))
+  
+(defn part1 [input]
+  (->> input
+       (mapcat (partial range->invalids 1))
+       (reduce +)))
+
+(defn part2 [input]
+  (->> input
+       (mapcat (partial range->invalids 2))
+       (reduce +)))
+
+(comment
+  (take 10 (parse input))
+
+  (def puzzle "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124")
+  (range->invalids (first (parse puzzle)))
+  (invalid2? 2121212118)
+  
+  (first (parse puzzle))
+
+  (time (part1 (parse puzzle)))
+  (part2 (parse puzzle))
+
+  )
+
+  (submit 2025 2 1 (time (part1 (parse input))))
+  (submit 2025 2 2 (part2 (parse input)))
+  
+  )
