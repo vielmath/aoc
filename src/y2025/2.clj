8188db9e203bf711b7e78adf35a44a2d250602bd
Mathieu <mathieu.marchandise@oscaro.com>
2025-12-04 22:21:26 +0100

    prettier 2025 2

--- src/y2025/2.clj
+++ src/y2025/2.clj
@@ -1,6 +1,6 @@
 (ns y2025.2
   (:require [clojure.string :as str]
-            [tools :refer [read-puzzle read-input submit]]))
+            [tools :refer [read-input submit]]))
 
 (def input (read-input 2025 2))
 
@@ -9,8 +9,7 @@
    (-> (str/split-lines raw)
        first
        (str/split #","))
-   (map (fn [range] (map Long/parseLong (str/split range #"-"))))
-   ))
+   (map (fn [range] (map Long/parseLong (str/split range #"-"))))))
 
 (defn invalid? [id]
   (let [s (str id)
@@ -24,16 +23,13 @@
     (->>
      (range 1 (inc (quot c 2)))
      (filter #(zero? (mod c %)))
-     ;(map #(partition-all % s))
      (map #(apply = (partition-all % s)))
-     (some true?)
-     )))
+     (some true?))))
 
 (defn range->invalids [part [from to]]
   (->> (range from (inc to))
-       (filter (if (= part 1) invalid? invalid2?))
-       ))
-  
+       (filter (if (= part 1) invalid? invalid2?))))
+
 (defn part1 [input]
   (->> input
        (mapcat (partial range->invalids 1))
@@ -48,31 +44,33 @@
   (take 10 (parse input))
 
   (def puzzle "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124")
-  (range->invalids (first (parse puzzle)))
+  (range->invalids 1 (first (parse puzzle)))
   (invalid2? 2121212118)
-  
+
   (first (parse puzzle))
-  (apply max (mapcat #(map (comp count str) %) (parse input)))
+
+  ;; let's try to solve it faster biginning by generating all the invalid ids
+  ;; spoiler - it is faster : 3 s to 3xx ms...
+  ;; first find the biggest value, no need to go further
+  (apply max (mapcat #(map (comp count str) %) (parse input))) ;; 10 char max
 
   (time
-  (let [candidates
-        (set (mapcat (fn [n] (let [c (str n)
-                              s (count c)]
-                          (->> (range 2 (inc (quot 10 s)))
-                               (map (fn [a] (Long/parseLong (apply str (repeat a c))))))))
-                (range 1 100000)))]
-    (reduce +
-            (mapcat (fn [[a b]] (filter #(<= a % b) candidates)) (parse input))
-             )
-    )
-  )
+   (let [candidates (set
+                     (mapcat (fn [n]
+                               (let [c (str n)
+                                     s (count c)]
+                                 (->> (range 2 (inc (quot 10 s)))
+                                      (map (fn [a] (Long/parseLong (apply str (repeat a c))))))))
+                             (range 1 100000)))]
+     (reduce +
+             (mapcat (fn [[a b]]
+                       (filter #(<= a % b) candidates))
+                     (parse input)))))
+
+
 
   (time (part1 (parse puzzle)))
   (part2 (parse puzzle))
 
-  )
-
   (submit 2025 2 1 (time (part1 (parse input))))
-  (submit 2025 2 2 (time (part2 (parse input))))
-  
-  )
+  (submit 2025 2 2 (time (part2 (parse input)))))
