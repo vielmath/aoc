c5e70aca93bf6d7bcfbfc52317a6b3985838d855
Mathieu <mathieu.marchandise@oscaro.com>
2025-12-06 22:00:31 +0100

    oups pushed without saving...

--- src/y2025/6.clj
+++ src/y2025/6.clj
@@ -4,7 +4,6 @@
 
 (def input (read-input 2025 6))
 
-
 (defn calc [opp]
   (eval
    (read-string
@@ -12,56 +11,47 @@
          (->> opp
               reverse
               (str/join " "))
-         ")")))
-  )
+         ")"))))
 
 (defn parse [raw]
   (let [lines (->> (str/split-lines raw)
-                   (map (fn [l] (remove str/blank? (str/split l #"\s+"))))
-                   )]
-       (partition (count lines) (apply interleave lines))
-
-       ))
+                   (map (fn [l] (remove str/blank? (str/split l #"\s+")))))]
+    (partition (count lines) (apply interleave lines))))
 
 (defn part1 [input]
   (reduce + (map calc input)))
 
-
-(defn cephalocalc [lines [[f opp][t _]]]
+(defn cephalocalc [lines [[f op] [t _]]]
   (let [nums (map #(subs % f (dec t)) lines)]
-       (partition (count nums) (apply interleave nums))
-       ))
+    (->> (partition (count nums) (apply interleave nums))
+         (map #(parse-long (str/trim (apply str %))))
+         (apply (if (= op \+) + *)))))
 
 (defn parse2 [raw]
-  (let [lines (->> (str/split-lines raw)
-                   )]
-    (->> lines
-         last
-         (map-indexed vector)
-         (remove #(= \space (second %)))
-         (partition 2 1)
-         (map (partial cephalocalc (butlast lines))))
+  (let [lines (str/split-lines raw)
+        ops (->> lines
+                 last
+                 (map-indexed vector)
+                 (remove #(= \space (second %)))
 
-    ))
+                 vec)]
+    (->> (conj ops [(inc (count (first lines))) \_])
+         (partition 2 1)
+         (map (partial cephalocalc (butlast lines))))))
 
 (defn part2 [input]
   (->> input
-       ;first
+       (reduce +)
        ;
        ))
 
 (comment
   (take 10 (parse input))
 
-  (first (parse (first (read-puzzle 2025 6)))
-         )
+  (first (parse (first (read-puzzle 2025 6))))
 
   (part1 (parse (first (read-puzzle 2025 6))))
-  (part2 (parse2 (first (read-puzzle 2025 6)))
-         )
+  (part2 (parse2 (first (read-puzzle 2025 6))))
 
   (submit 2025 6 1 (part1 (parse input)))
-  (submit 2025 6 2 (part2 (parse2 input))
-          )
-  
-  )
+  (submit 2025 6 2 (part2 (parse2 input))))
