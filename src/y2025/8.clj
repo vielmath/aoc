8e8527bc569fe5132835b59f3f80ab4df5aa4152
Mathieu <mathieu.marchandise@oscaro.com>
2025-12-08 16:46:21 +0100

    2025 8 part 2

--- src/y2025/8.clj
+++ src/y2025/8.clj
@@ -1,8 +1,9 @@
 (ns y2025.8
-  (:require [clojure.string :as str]
-            [clojure.math :as math]
-            [tools :refer [read-puzzle read-input submit]]
-            ))
+  (:require
+   [clojure.math :as math]
+   [clojure.string :as str]
+   [clojure.set :as s]
+   [tools :refer [read-input read-puzzle submit]]))
 
 (def input (read-input 2025 8))
 
@@ -20,15 +21,14 @@
 
 (defn parse [raw]
   (->> (str/split-lines raw)
-       (map #(map parse-long (str/split % #",")))
-       ))
+       (map #(map parse-long (str/split % #",")))))
 
 (defn pairs [coll]
   (set
    (for [y coll
-        x coll
-        :when (not (= x y))]
-    #{x y})))
+         x coll
+         :when (not (= x y))]
+     #{x y})))
 
 (defn circuits [current p c]
   ;(println current)
@@ -39,44 +39,58 @@
       ; 2 dans le meme groupe - noop
       (if (and (contains? circuit p)
                (contains? circuit c))
-        circuits
+        current
         (if (or (contains? circuit p)
                 (contains? circuit c))
           (if match
             ;; 2 dans 2 groupes
-            (conj (into res (rest remaining)) (clojure.set/union match circuit))
-            (recur (rest remaining) circuit res)
-            )
-          (recur (rest remaining) match (conj res circuit))
+            (conj (into res (rest remaining)) (s/union match circuit))
+            (recur (rest remaining) circuit res))
+          (recur (rest remaining) match (conj res circuit))))
 
-        ))
-
-      ; 1 dans 1 groupe
+; 1 dans 1 groupe
       ; 2 dans 2 groupes
-      (if (or (contains? circuit p)
-              (contains? circuit c))
-        (conj (into res (rest remaining)) (conj circuit p c))
-        (recur (rest remaining) (conj res circuit)))
-      (conj res #{p c}))))
+      ;(if (or (contains? circuit p)
+      ;        (contains? circuit c))
+      ;  (conj (into res (rest remaining)) (conj circuit p c))
+      ;(recur (rest remaining) (conj res circuit)))
+      (if match
+        ;; 1 dans 1 groupe
+        (conj res (conj match p c))
+        ;; no match
+        (conj res #{p c})))))
 
 (defn part1 [input]
   (->>
    (pairs input)
    (map (fn [pair] [pair (apply euclidian-distance pair)]))
    (sort-by second)
-   (take 10)
-   ;(reduce (fn [acc [pair _]]
-   ;         (apply circuits acc pair))
-   ;       [])
-   ;(map count)
-   ;(sort >)
-   ;(take 3)
-   ;(apply *)
-   ))
+   (take 1000)
+   (reduce (fn [acc [pair _]]
+             (apply circuits acc pair))
+           [])
+   (map count)
+   (sort >)
+   (take 3)
+   (apply *)))
 
 (defn part2 [input]
-  (->> input
-       ))
+  (let [sorted-pairs (->> (pairs input)
+                          (map (fn [pair] [pair (apply euclidian-distance pair)]))
+                          (sort-by second))]
+
+    (->>
+     (loop [remaining-pairs sorted-pairs
+            state (mapv (fn [p] #{p}) input)]
+       (if-let [[pair _] (first remaining-pairs)]
+         (let [res (apply circuits state pair)]
+           (println (count res))
+           (if (= 1 (count res))
+             pair
+             (recur (rest remaining-pairs) res)))
+         "oups no more pairs"))
+     (map first)
+     (apply *))))
 
 (comment
   (count (parse input))
@@ -86,7 +100,5 @@
   (part1 (parse (first (read-puzzle 2025 8))))
   (part2 (parse (first (read-puzzle 2025 8))))
 
-  (submit 2025 8 1 (part1 (parse input)))
-  (submit 2025 8 2 (part2 (parse input)))
-  
-  )
+  (submit 2025 8 1 (time (part1 (parse input))))
+  (submit 2025 8 2 (time (part2 (parse input)))))
